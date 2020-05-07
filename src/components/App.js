8a3a0c294f1981aa0d76aaddac4852af243b6d72
Mohit Bajaj <mohit_b35@hotmail.com>
2020-05-07 17:10:19 +0530

    create scaffolding for routes - wire up routes - create route components

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'; 
 
-const Page1 = () => {
-	return (
-		<div>
-			Page1
-			<Link to="/page2">Navigate to page 2</Link>
-		</div>
-		
-	)
-};
-
-const Page2 = () => {
-	return (
-		<div>
-			Page2
-			<Link to="/">Navigate to page 1</Link>
-		</div>
-
-	)
-};
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
 
 const App = () => {
 	return (
 		<div>
 			<BrowserRouter>
-				<Route path="/" exact component={Page1} />
-				<Route path="/page2" component={Page2} />
+				<Route path="/" exact component={StreamList}/>
+				<Route path="/streams/show" exact component={StreamShow}/>
+				<Route path="/streams/new" exact component={StreamCreate}/>
+				<Route path="/streams/edit" exact component={StreamEdit}/>
+				<Route path="/streams/delete" exact component={StreamDelete}/>
 			</BrowserRouter>
 		</div>
 	)
