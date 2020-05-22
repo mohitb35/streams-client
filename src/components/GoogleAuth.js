2816912df26d21c1291033651f7fc3da69ddc74e
Mohit Bajaj <mohit_b35@hotmail.com>
2020-05-22 16:37:34 +0530

    add rough Authentication flow (No Redux) 1. Get instance of auth object 2. Set state with value of auth.isSignedIn. 3. Set up listener to update state on change to sign-in state 4. Conditional rendering a. isSignedIn null (initial state), b. isSignedIn true -> Sign Out button c. isSignedIn false -> Sign In button

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,19 +1,64 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+	state = {
+		isSignedIn: null
+	};
 
 	componentDidMount() {
 		window.gapi.load('client:auth2', () => {
 			window.gapi.client.init({
 				clientId: process.env.REACT_APP_CLIENT_ID,
 				scope: 'email'
+			}).then(() => {
+				//Get auth object, and set state with isSignedIn
+				this.auth = window.gapi.auth2.getAuthInstance();
+				this.setState({
+					isSignedIn: this.auth.isSignedIn.get()
+				});
+				this.auth.isSignedIn.listen(this.onAuthChange);
 			})
 		});
 	};
 
+	onAuthChange = (isSignedIn) => {
+		this.setState({
+			isSignedIn: isSignedIn
+		})
+	};
+
+	onSignIn = () => {
+		this.auth.signIn();
+	}
+
+	onSignOut = () => {
+		this.auth.signOut();
+	}
+
+	renderAuthButton() {
+		// Check is user is signed in (based on state)
+		if (this.state.isSignedIn === null){
+			return null;
+		} else if (this.state.isSignedIn) {
+			return (
+				<button className="ui red google button" onClick={this.onSignOut}>
+					<i className="google icon" />
+					Sign Out
+				</button>
+			);
+		} else {
+			return(
+				<button className="ui red google button" onClick={this.onSignIn}>
+					<i className="google icon" />
+					Sign In With Google
+				</button>
+			);
+		}
+	}
+
 	render() {
 		return (
-			<div>Google Auth</div>
+			<div>{this.renderAuthButton()}</div>
 		)
 	}
 }
