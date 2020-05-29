fa78e4d4d905a6ea8d4f4d33d451e0138275b6f8
Mohit Bajaj <mohit_b35@hotmail.com>
2020-05-29 12:59:28 +0530

    add redux form - step 2 1. configure reduxForm a. import reduxForm, and use to wrap component while exporting b. reduxForm provides props to wrapped component e.g. "form" c. props can be configured, or automatically provided 2. configure Fields a. import Field (react component) b. use Field as react component in render function/functional component c. Field must be provided with following props i. name (reqd) - corresponds to name in store ii. component (reqd) - can be input/textArea/select, class, or function - formProps are available to functions, containing value, event handlers - wire value/event handlers within returned JSX for controlled component

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+// Field = react component, reduxForm = function
 
-const StreamCreate = () => {
-	return (
-		<div>StreamCreate</div>
-	)
+class StreamCreate extends React.Component {
+
+	renderInput(/*formProps*/{ input }) {
+		// console.log(formProps);
+		// props sent to component within field
+		/* return (
+			<input 
+				type="text" 
+				onChange={formProps.input.onChange} 
+				value={formProps.input.value}	
+			/>
+		) */
+		return (
+			<input 
+				type="text" 
+				/* {...formProps.input} */
+				{ ...input }
+				//Shortform representation to include onChange and Value. 
+				// You can also destructure input from formProps in the function params, or use ...formProps.input
+			/>
+		);
+	}
+
+	render() {
+		// console.log(this.props); 
+		// props are made available by wrapping the component in reduxForm
+		return (
+			<form>
+				<Field name="title" component={this.renderInput}/>
+				<Field name="description" component={this.renderInput}/>
+			</form>
+		)
+	}
+
 }
 
-export default StreamCreate;
+export default reduxForm({
+	form: 'streamCreate'
+})(StreamCreate);
