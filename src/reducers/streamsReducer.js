200a66b568e549841752616ca0efc4b4560b5979
Mohit Bajaj <mohit_b35@hotmail.com>
2020-06-08 11:42:42 +0530

    add reducers in bulk 1. config - import streamsReducer and export within combineReducers 2. create streamsReducer to handle following actions a. FETCH_STREAM -> Add/merge fetched stream details to current streams b. CREATE_STREAM -> Add created stream details to current streams c. EDIT_STREAM -> Merge updated stream details to current streams d. FETCH_STREAMS -> Merge fetched streams [ ] with streams obj in store e. DELETE_STREAM -> Remove stream from streams and update streams obj

---
+++ src/reducers/streamsReducer.js
@@ -0,0 +1,38 @@
+import {
+	CREATE_STREAM,
+	FETCH_STREAM,
+	FETCH_STREAMS,
+	EDIT_STREAM,
+	DELETE_STREAM,
+} from '../actions/types';
+
+// state = streams for this reducer
+export default (state = {}, action) => {
+	switch (action.type) {
+		case FETCH_STREAM:
+			return { ...state, [action.payload.id]: action.payload };
+			
+		case CREATE_STREAM:
+			return { ...state, [action.payload.id]: action.payload };
+			
+		case EDIT_STREAM:
+			return { ...state, [action.payload.id]: action.payload };
+			
+		case FETCH_STREAMS:
+			return { 
+				...state,
+				...action.payload.reduce(
+					(streamMap, item) => ({...streamMap, [item.id]: item }),
+					{}
+				) 
+			};
+		
+		case DELETE_STREAM:
+			const newState = { ...state };
+			delete newState[action.payload];
+			return newState;
+
+		default:
+			return state;
+	}
+}
