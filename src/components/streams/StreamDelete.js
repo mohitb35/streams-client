efc458ddb270bda6c84fa79552e14f1d3adb1cc8
Mohit Bajaj <mohit_b35@hotmail.com>
2020-06-11 12:43:50 +0530

    updt StreamDelete > use React.Fragment in actions - done to avoid adding extra div around buttons, which affects CSS

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -3,21 +3,21 @@ import Modal from '../Modal';
 import history from '../../history';
 
 const StreamDelete = () => {
-	/* const actions = (
+	const actions = (
 		<React.Fragment>
 			<button className="ui button">Cancel</button>
 			<button className="ui button negative">Delete</button>
 		</React.Fragment>
-	); */
+	);
 
 	// Using a <div> tag as below, the modal css gets affected, but we can't just send the buttons back as they must be wrapped within some tags
 	// Alternative - use React.Fragment
-	const actions = (
+	/* const actions = (
 		<div>
 			<button className="ui button" onClick={()=>{history.push('/')}}>Cancel</button>
 			<button className="ui button negative">Delete</button>
 		</div>
-	);
+	); */
 
 	return (
 		<div>
